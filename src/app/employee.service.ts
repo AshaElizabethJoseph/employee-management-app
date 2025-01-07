a756b4b9b9ba13f756eca5ca57c6455e2823a193
ashaelizabethjoseph <ashacep38@gmail.com>
2025-01-07 13:50:28 +0530

    create employee service file to invoke apis

---
+++ src/app/employee.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../environments/environment';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiUrl = `${environment.apiUrl}/employees`;
+  constructor(private http:HttpClient) { }
+
+  getEmoloyees(): Observable<Employee[]>{
+    return this.http.get<Employee[]>(this.apiUrl);
+  }
+
+}
